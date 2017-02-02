e30a59be1fb3a537f03ffbc16f13d1e776b96a3d
virtuNat <janlothan@gmail.com>
2017-02-03 01:55:17 +0800

    Additional fix

--- short_spiro.py
+++ short_spiro.py
@@ -8,6 +8,6 @@ def G(s,_r):
 	if r==0:raise ZeroDivisionError
 	f=(h if h<k else k)/((abs(R+o)+r)*1.2)
 	return[[((R+r)*_c(t)+o*_c(((R+r)*(t)/r)))*f+h,((R+r)*_s(t)+o*_s(((R+r)*(t)/r)))*f+k]for t in range(int(360*float(r)/g(R,r)))]
-_S = P.Surface((100,100))
+_S=P.Surface((100,100))
 P.draw.aalines(_S,(0, 255, 0),True,G(_S.get_size(),(10,7,7)))
 P.image.save(_S,'spiro.png')
